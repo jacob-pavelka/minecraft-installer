2b8a27ac9516c18fea65b06f01db6fe9b450e265
jacobpavelka <86691427+jacobpavelka@users.noreply.github.com>
2023-05-12 21:36:25 -0500

    added server mods

--- server_install.py
+++ server_install.py
@@ -24,12 +24,16 @@ for mod in mods['mods']:
     if mod['server'] == True:
         try:
             out_path = os.path.join(server_dir,"mods",f"{mod['name']}.jar")
-            # jar = wget.download(mod['19.2']['url'], out=f"{server_dir}/mods/{mod['name']}.jar")
+            if os.path.exists(out_path):
+                continue
+            jar = wget.download(mod['19.2']['url'], out=f"{server_dir}/mods/{mod['name']}.jar")
         except KeyError:
             print(f'this mod ({mod["name"]}) is not supported on the current version')
 for dp in mods['datapacks']:
     if dp['server'] == True:
         try:
+            if os.path.exists(out_path):
+                continue
             out_path = os.path.join(server_dir,get_world_name(server_dir),"datapacks",f"{dp['name']}.jar")
             jar = wget.download(dp['19.2']['url'], out=out_path)
         except KeyError:
